38795e1f107be167bc3f2faf3957a92ebd306455
aumie <aum5376@gmail.com>
2020-05-29 15:02:30 +0700

    first time

---
+++ cmake-build-debug/test/CMakeFiles/unit_tests.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ndrs/Desktop/gtest-demo/test/example_add.cpp" "/home/ndrs/Desktop/gtest-demo/cmake-build-debug/test/CMakeFiles/unit_tests.dir/example_add.cpp.o"
+  "/home/ndrs/Desktop/gtest-demo/test/example_subtract.cpp" "/home/ndrs/Desktop/gtest-demo/cmake-build-debug/test/CMakeFiles/unit_tests.dir/example_subtract.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest/googletest-src/googletest/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ndrs/Desktop/gtest-demo/cmake-build-debug/googletest/googletest-build/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/ndrs/Desktop/gtest-demo/cmake-build-debug/src/CMakeFiles/example.dir/DependInfo.cmake"
+  "/home/ndrs/Desktop/gtest-demo/cmake-build-debug/googletest/googletest-build/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ndrs/Desktop/gtest-demo/test
+# Build directory: /home/ndrs/Desktop/gtest-demo/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit "/home/ndrs/Desktop/gtest-demo/cmake-build-debug/bin/unit_tests")
+set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/home/ndrs/Desktop/gtest-demo/test/CMakeLists.txt;13;add_test;/home/ndrs/Desktop/gtest-demo/test/CMakeLists.txt;0;")
